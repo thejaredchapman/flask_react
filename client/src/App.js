657301a0e09615cc138d86b13bdc8190a2cda322
Jared Chapman <thejaredchapman@gmail.com>
2024-06-12 18:50:39 -0500

    progress so far, need to fix return div in App REACT

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import React, { useState, useEffect } from 'react'
 
 function App() {
-  return(
+
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  },[])
+
+  return (
     <div>
 
+        {(typeof data.dogs === 'undefined') ? (
+          <p>Loading...</p>
+        ) : (
+          data.dogs.map(dog, i) => (
+              <p key={i}>{dogs}</p>
+          ))
+        )}
+      
     </div>
   )
 }
 
-export default App;
+export default App
